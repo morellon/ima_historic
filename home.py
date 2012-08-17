f4e896585a6508ae832d9843ced200170854f9be
Thiago Morello <thiago.morello@locaweb.com.br>
2012-08-17 11:08:23 -0300

    Returning json data

--- home.py
+++ home.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 from google.appengine.ext import db
-from models import HistoricData
+from models import HistoricDatum, FinancialAsset, JSONEncoder
 from datetime import date, timedelta
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import logging
+import logging, urllib, time
 import anbima
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        pass
+class HistoricDataPage(webapp.RequestHandler):
+    def get(self, code):
+        code = urllib.unquote_plus(code)
+        asset = db.GqlQuery("SELECT * FROM FinancialAsset WHERE code = :code", code=code).get()
+        logging.error("Asset: %s" % code)
+        if not asset:
+            data = []
+        else:
+            data = asset.historic_data.order("date")
+
+        series = [[datum.date, datum.value] for datum in data]
+        self.response.out.write(JSONEncoder().encode(series))
 
 class CronPage(webapp.RequestHandler):
     def get(self):
@@ -18,16 +27,28 @@ class CronPage(webapp.RequestHandler):
         today = date.today()
         for i in range(6):
             historic_data = anbima.get_indexes(today - i*day)
-            if "IMA-B TOTAL" in historic_data:
-                data = HistoricData()
-                data.date = historic_data["IMA-B TOTAL"]["date"]
-                data.value = historic_data["IMA-B TOTAL"]["value"]
-                data.put()
+            code = "IMA-B TOTAL"
+            if code in historic_data:
+                asset = db.GqlQuery("SELECT * FROM FinancialAsset WHERE code = :code", code=code).get()
+                if not asset:
+                    asset = FinancialAsset()
+                    asset.name = code
+                    asset.code = code
+                    asset.put()
+
+                index_data = historic_data[code]
+                data = db.GqlQuery("SELECT * FROM HistoricDatum WHERE date = :date", date=index_data["date"]).get()
+                if not data:
+                    data = HistoricDatum()
+                    data.date = index_data["date"]
+                    data.value = index_data["value"]
+                    data.financial_asset = asset
+                    data.put()
         pass
 
 application = webapp.WSGIApplication(
     [
-        ('/ima-b', MainPage),
+        (r'/assets/(.+)/data', HistoricDataPage),
         ('/tasks/update', CronPage)
     ],
     debug=True
--- models.py
+++ models.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 from google.appengine.ext import db
+import datetime, time
+from django.utils import simplejson as json
 
-class FinancialAsset(db.Model):
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return int(time.mktime(obj.timetuple()))
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+class BASE(db.Model):
+    def to_json(self):
+        return JSONEncoder().encode(self.to_dict())
+
+    def to_dict(self):
+        return db.to_dict(self)
+
+
+class FinancialAsset(BASE):
     name = db.StringProperty(multiline=False)
     code = db.StringProperty(multiline=False)
 
-class HistoricData(db.Model):
+
+class HistoricDatum(BASE):
+    financial_asset = db.ReferenceProperty(FinancialAsset,
+                                           collection_name='historic_data')
     date = db.DateTimeProperty(auto_now_add=False)
     value = db.FloatProperty()
+
+    def to_dict(self):
+        return {
+            'date': self.date,
+            'value': self.value
+        }
